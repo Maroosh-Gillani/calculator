07f815ef9228892131c7ba2468fe505b6efad7b9
Maroosh-Gillani <76667567+Maroosh-Gillani@users.noreply.github.com>
2023-07-13 23:53:04 -0400

    Added basic button functionality for all buttons except for the clear button. Clicked buttons displayed on screen.

--- script.js
+++ script.js
@@ -1,39 +1,84 @@
-// These will probably come from query selector
-let number1, operator, number2, displayValue;
+document.addEventListener('DOMContentLoaded', function () {
 
-// Basic Operations
-function add(a, b) {
-    return a + b;
-}
+    const screen = document.querySelector('.screen');
+    const numberButtons = document.querySelectorAll('.num');
+    const operatorButtons = document.querySelectorAll('.op');
+    const equalButton = document.querySelector('.equalButton');
+    let firstNumber = '';
+    let selectedOperator = '';
 
-function subtract(a, b) {
-    return a - b;
-}
+    // Handle number button clicking
+    numberButtons.forEach(numberButton => {
+        numberButton.addEventListener('click', function () {
+            console.log(this.value);
+            screen.textContent += this.value;
+        });
+    });
 
-function multiply(a, b) {
-    return a * b;
-}
+    // Handle operator button clicking
+    operatorButtons.forEach(operatorButton => {
+        operatorButton.addEventListener('click', function () {
+            firstNumber = screen.textContent; // Take the numbers from before the operator
+            selectedOperator = this.value;
+            screen.textContent += this.value;
 
-function divide(a, b) {
-    return a/b;
-}
+            // Disable operators if one has already been selected on screen
+            operatorButtons.forEach(button => {
+                button.disabled = true;
+            });
 
-function operate(num1, op, num2) {
-    if (op == '+') {
-        return add(num1, num2);
+        });
+    });
+
+    // Handle equal button clicking
+    equalButton.addEventListener('click', function () {
+        const secondNumber = screen.textContent.slice(firstNumber.length + 1);
+        const result = operate(parseFloat(firstNumber), selectedOperator, parseFloat(secondNumber));
+
+        screen.textContent = result;
+        selectedOperator = '';
+
+        // Enable operations again
+        operatorButtons.forEach(button => {
+            button.disabled = false;
+        });
+    });
+
+
+    // Basic Operations
+    function add(a, b) {
+        return a + b;
     }
-    if (op == '-') {
-        return subtract(num1, num2);
+
+    function subtract(a, b) {
+        return a - b;
     }
-    if (op == '*') {
-        return multiply(num1, num2);
+
+    function multiply(a, b) {
+        return a * b;
     }
-    if (op == '/') {
-        if (num2 == 0) {
-            alert("Can't divide by 0.");
+
+    function divide(a, b) {
+        return a / b;
+    }
+
+    function operate(num1, op, num2) {
+        if (op == '+') {
+            return add(num1, num2);
         }
-        else {
-            return divide(num1, num2);
+        if (op == '-') {
+            return subtract(num1, num2);
+        }
+        if (op == '*') {
+            return multiply(num1, num2);
+        }
+        if (op == '/') {
+            if (num2 == 0) {
+                alert("Can't divide by 0.");
+            }
+            else {
+                return divide(num1, num2);
+            }
         }
     }
-}
+});
